7292522c4b275f3acd2c9ac043934a8a9cbcdb6e
rafael <rafael@rafaels-MacBook-Pro.local>
2018-01-08 21:45:12 -1000

    completed exercise 7

--- exercises.js
+++ exercises.js
@@ -149,7 +149,15 @@ console.log(isEqual("Cher","Sally"));
  * If the strings are not equal, the function will return the message "Opposites do attract." Otherwise, return the message: "Cause it's like you're my mirror." 
  * Console.log your result.
 */
-
+function notEqual(first, second){
+	if (first !== second){
+		return "Opposites do attract.";
+	}else{
+		return "Cause it's like you're my mirror";
+	}
+	
+}
+console.log(notEqual("asteroid, chair"));
 
 /*
  * #8
