35da72eeea911636bd060d698d4d5a3d2e79a04b
rafael <rafael@rafaels-MacBook-Pro.local>
2018-01-08 21:51:35 -1000

    completed exercise 8

--- exercises.js
+++ exercises.js
@@ -170,7 +170,14 @@ console.log(notEqual("asteroid, chair"));
  * The function will return true if the number passed into the function is greater than 100, otherwise it will return false.
  * Console.log your result.
 */ 
-
+function spareChange(money){
+	if (money > 100){
+		return true;
+	}else{
+		return false;
+	}
+}
+console.log(spareChange(3));
 
 
 /*
