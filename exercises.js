b52e789e623fe528ebbd130ff281cb7f2d0d2fe4
rafael <rafael@rafaels-MacBook-Pro.local>
2018-01-08 22:03:09 -1000

    Completed exercise 11

--- exercises.js
+++ exercises.js
@@ -235,8 +235,14 @@ console.log(evenStevens(2));
  * If BOTH values are 21 or over, the function will return the message: "Welcome to the Legends Lounge." Otherwise, it will return the message: "Chuck E Cheese is across the street."
  * Console.log your result.
 */ 
-
-
+function daClub(cover, age){
+	if (cover >= 21 && age >= 21){
+		return "Welcome to the Legends Lounge.";
+	}else{
+		return "Chuck E Cheese is across the street.";
+	}
+}
+console.log(daClub(5, 5));
 /*
  * #12
  * Function - graduation
