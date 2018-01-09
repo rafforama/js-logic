b057b3806335ce089ef88797774e907e5c3a4bbe
rafael <rafael@rafaels-MacBook-Pro.local>
2018-01-08 21:28:38 -1000

    completed exercise 4

--- exercises.js
+++ exercises.js
@@ -85,8 +85,15 @@ console.log(isGreaterThan(1,2));
  * The function will return true if the value passed into the function is "true".
  * Console.log your result.
 */
-
-
+function mustBeTrue(boo){
+	if (boo === true){
+		return true;
+	}else{
+		return false;
+	}
+	
+}
+console.log(mustBeTrue(true));
 
 /*
  * #5
